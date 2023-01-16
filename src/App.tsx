f5c2a09de9b201583cb6b78183e333226f72cd10
Peter Yoo <trustintofu@gmail.com>
2023-01-16 01:51:57 -0800

    feat: Adds env variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3001/');
+const socket = io(import.meta.env.VITE_APP);
 
 const App = () => {
   const [isConnected, setIsConnected] = useState<any>(socket.connected);
