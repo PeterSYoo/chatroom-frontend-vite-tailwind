c33968c2f148ad56add70cd976df50c92dec7d96
Peter Yoo <trustintofu@gmail.com>
2023-01-17 00:43:05 -0800

    style: Adds overflow on y axis for users list.

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,4 +1,5 @@
 import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 type Socket = {
   socket: any;
@@ -8,6 +9,12 @@ type Socket = {
 
 const Chat = ({ room, username, socket }: Socket) => {
   const [currentMessage, setCurrentMessage] = useState<string>('');
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate('/');
+  };
 
   const sendMessage = async () => {
     if (currentMessage !== '') {
@@ -28,18 +35,21 @@ const Chat = ({ room, username, socket }: Socket) => {
   return (
     <>
       <div className="h-screen min-w-screen bg-gray-900">
-        <div className="w-full h-full px-20 py-20 flex flex-col justify-center items-center">
+        <div className="w-full h-full px-20 py-48 flex flex-col justify-center items-center">
           <div className="w-full h-full flex flex-col p-3 gap-5 justify-center">
             {/* Header */}
-            <div className="h-4 w-full flex justify-between items-center">
+            <div className="h-0 w-full flex justify-between items-center px-4">
               <p className="text-gray-400">Chat Room</p>
-              <button className="text-gray-400 hover:text-white px-4 py-1">
+              <button
+                onClick={handleLogout}
+                className="text-gray-400 hover:text-white"
+              >
                 Logout
               </button>
             </div>
             {/* Grid Container */}
             <div className="border border-gray-700 p-2 grid grid-cols-12 h-full bg-gray-800 rounded-md">
-              <div className="col-start-1 col-span-10 border border-gray-700 p-2 text-white overflow-y-auto scrollbar-hide">
+              <div className="col-start-1 col-span-10 border border-gray-700 p-2 text-gray-200 overflow-y-auto scrollbar-hide">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                 Adipisci fugiat unde illo beatae eum sed animi deserunt aliquam
                 quasi expedita eveniet voluptates eligendi enim dolores quia
@@ -85,22 +95,69 @@ const Chat = ({ room, username, socket }: Socket) => {
                 aperiam culpa voluptatibus architecto itaque nisi est officiis,
                 voluptatum nesciunt suscipit quas harum?
               </div>
-              <div className="col-start-11 col-span-2 border-r border-b border-t border-gray-700 text-gray-400 flex justify-center">
-                <div className="w-full flex flex-col">
-                  <div className="w-full border-b border-gray-700 h-fit flex justify-center pt-2 pb-1">
+              <div className="col-start-11 col-span-2 border-r border-b border-t border-gray-700 text-gray-400 flex justify-center overflow-y-auto scrollbar-hide">
+                <div className="w-full flex flex-col overflow-y-auto scrollbar-hide">
+                  <div className="w-full border-b border-gray-700 h-fit flex justify-center pt-2 pb-1 text-sm">
                     Users
                   </div>
-                  <div className="py-2 px-2">&#x2022; Apple</div>
+                  <div className="py-2 px-2 text-white">
+                    &#x2022; Apple Lorem ipsum dolor, sit amet consectetur
+                    adipisicing elit. Adipisci fugiat unde illo beatae eum sed
+                    animi deserunt aliquam quasi expedita eveniet voluptates
+                    eligendi enim dolores quia dignissimos repudiandae, vero
+                    nostrum. Lorem ipsum, dolor sit amet consectetur adipisicing
+                    elit. Voluptatibus doloremque modi iusto in voluptate ex
+                    earum aliquid cum, neque id, quae non atque minima ratione
+                    mollitia quidem, cupiditate tempore officia! Lorem ipsum
+                    dolor sit amet consectetur adipisicing elit. Labore,
+                    necessitatibus. Adipisci cum quo, explicabo, quam itaque
+                    officia quas molestiae quae possimus deserunt nisi
+                    consectetur, repellendus fugiat voluptates magnam
+                    accusantium a? Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Natus ipsa et dicta totam aperiam officiis
+                    dolore libero enim quae veniam laudantium, quisquam quaerat
+                    quis aspernatur minima illum molestiae. Cum, odio! Lorem
+                    ipsum dolor sit amet consectetur adipisicing elit. Nihil,
+                    sit, quisquam eveniet eius id officiis quos cupiditate
+                    ratione facilis, omnis corrupti mollitia recusandae ab quis
+                    quasi modi reiciendis magnam. Magnam. Lorem ipsum dolor sit
+                    amet consectetur, adipisicing elit. Rem quis perspiciatis
+                    facere porro expedita, recusandae obcaecati placeat aperiam
+                    culpa voluptatibus architecto itaque nisi est officiis,
+                    voluptatum nesciunt suscipit quas harum? Lorem ipsum dolor,
+                    sit amet consectetur adipisicing elit. Adipisci fugiat unde
+                    illo beatae eum sed animi deserunt aliquam quasi expedita
+                    eveniet voluptates eligendi enim dolores quia dignissimos
+                    repudiandae, vero nostrum. Lorem ipsum, dolor sit amet
+                    consectetur adipisicing elit. Voluptatibus doloremque modi
+                    iusto in voluptate ex earum aliquid cum, neque id, quae non
+                    atque minima ratione mollitia quidem, cupiditate tempore
+                    officia! Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit. Labore, necessitatibus. Adipisci cum quo, explicabo,
+                    quam itaque officia quas molestiae quae possimus deserunt
+                    nisi consectetur, repellendus fugiat voluptates magnam
+                    accusantium a? Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Natus ipsa et dicta totam aperiam officiis
+                    dolore libero enim quae veniam laudantium, quisquam quaerat
+                    quis aspernatur minima illum molestiae. Cum, odio! Lorem
+                    ipsum dolor sit amet consectetur adipisicing elit. Nihil,
+                    sit, quisquam eveniet eius id officiis quos cupiditate
+                    ratione facilis, omnis corrupti mollitia recusandae ab quis
+                    quasi modi reiciendis magnam. Magnam. Lorem ipsum dolor sit
+                    amet consectetur, adipisicing elit. Rem quis perspiciatis
+                    facere porro expedita, recusandae obcaecati placeat aperiam
+                    culpa voluptatibus architecto itaque nisi est officiis,
+                    voluptatum nesciunt suscipit quas harum?
+                  </div>
                 </div>
               </div>
             </div>
             {/* Chat Footer */}
             <div className="grid grid-cols-12">
               <div className="col-start-1 col-span-10 mr-4 border border-gray-700 p-2 bg-gray-800 rounded-md">
-                <input
-                  type="text"
+                <textarea
                   placeholder="Enter message"
-                  className="p-2 w-full bg-gray-800 border border-gray-700 focus:outline-none text-white placeholder:text-gray-500"
+                  className="p-2 px-3 w-full bg-gray-800 border border-gray-700 focus:outline-none text-white placeholder:text-gray-500"
                   onChange={(e) => setCurrentMessage(e.target.value)}
                 />
               </div>
